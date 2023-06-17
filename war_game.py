8ba97ac04a8b04c102bfc58bdfb570c24ac71e2f
Simonas kuprys <kuprys.simonas@gmail.com>
2023-06-17 14:25:28 +0300

    playing around

--- war_game.py
+++ war_game.py
@@ -1,3 +1,22 @@
-class Deck_of_cards:
-    
+import csv
 
+class Deck_of_cards:
+    def __init__(self, cards):
+        self.cards = cards
+        
+def main():
+    read_all_cards()
+
+
+
+
+def read_all_cards():
+    cards = []
+    with open("all_cards.csv") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            cards.append(row["card"])
+    print(cards)        
+        
+if __name__ == "__main__":
+    main()
